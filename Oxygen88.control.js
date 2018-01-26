0530aab4948b9dfe5849977be90076cda779f7af
boalint <boalint@gmail.com>
2018-01-26 18:02:18 +0100

    Faders as drawbars when shift is on

--- Oxygen88.control.js
+++ Oxygen88.control.js
@@ -1,181 +1,231 @@
 loadAPI(2);
 
-host.defineController("M-Audio", "Oxygen 88", "1.0", "7C290E15-04A8-4A16-AD73-96ECF03858E4");
-
-host.defineMidiPorts(1, 1);
-
-host.addDeviceNameBasedDiscoveryPair(["Oxygen 88"], ["Oxygen 88"]);
-
-// Search after different naming-schemes for autodetection
-for ( var i = 1; i < 9; i++)
-{
-	var name = i.toString() + "- Oxygen 88";
-	host.addDeviceNameBasedDiscoveryPair([name], [name]);
-	host.addDeviceNameBasedDiscoveryPair(["Oxygen 88 MIDI " + i.toString()], ["Oxygen 88 MIDI " + i.toString()]);
-}
-
-var TRANS =
-{
-	PREV_TRACK : 110,
-	NEXT_TRACK : 111,
-	LOOP       : 113,
-	REWIND     : 114,
-	FORWARD    : 115,
-	STOP       : 116,
-	PLAY       : 117,
-	RECORD     : 118
+const OXYGEN_88 = {
+    VENDOR          : 'M-Audio',
+    NAME            : 'Oxygen 88',
+    VERSION         : '1.4',
+    UUID            : '7C290E15-04A8-4A16-AD73-96ECF03858E4',
+    INPUT_COUNT     : 1,
+    OUTPUT_COUNT    : 1
 };
 
-var LOWEST_FADER = 33;
-var HIGHEST_FADER = 40;
-var MASTER_FADER = 41;
+const MIDI = 'MIDI';
 
-var LOWEST_ARM = 49;
-var HIGHEST_ARM = 56;
-var SHIFT_BUTTON = 57;
+host.defineController(OXYGEN_88.VENDOR, OXYGEN_88.NAME, OXYGEN_88.VERSION, OXYGEN_88.UUID);
+host.defineMidiPorts(OXYGEN_88.INPUT_COUNT, OXYGEN_88.OUTPUT_COUNT);
+host.addDeviceNameBasedDiscoveryPair([OXYGEN_88.NAME], [OXYGEN_88.NAME]);
 
-var LOWEST_KNOB = 17;
-var HIGHEST_KNOB = 24;
+// Search after different naming-schemes for autodetection
+for ( var i = 1; i < 9; i++) {
+    var strI = i.toString();
+    
+    var name1 = strI + '- ' + OXYGEN_88.NAME;
+    host.addDeviceNameBasedDiscoveryPair([name1], [name1]);
+    
+    var name2 = OXYGEN_88.NAME + ' ' + MIDI + ' ' + strI;
+    host.addDeviceNameBasedDiscoveryPair([name2], [name2]);
+}
+
+const TRANSPORT = {
+    PREV_TRACK      : 110,
+    NEXT_TRACK      : 111,
+    LOOP            : 113,
+    REWIND          : 114,
+    FORWARD         : 115,
+    STOP            : 116,
+    PLAY            : 117,
+    RECORD          : 118
+};
+
+const FADER = {
+    LOW             : 33,
+    HIGH            : 40,
+    COUNT           : 8,
+    
+    MASTER          : 41
+};
+
+const KNOB = {
+    LOW             : 17,
+    HIGH            : 24,
+    COUNT           : 8
+};
+
+const BUTTON = {
+    LOW             : 49,
+    HIGH            : 56,
+    SHIFT           : 57
+}
+
+const MESSAGES = {
+    ON_OFF          : ['OFF', 'ON'],
+    MUTE            : ['MUTED', 'LOUD'],
+};
+
+const BUTTON_MESSAGE_MAP = [MESSAGES.ON_OFF, MESSAGES.MUTE];
+
+const D2 = 128;
+const S_CC_UNDEFINED = 176;
+
+const CC_BREATH = 2;
+const CC_EXPRESSION = 11;
+const CC_SUSTAIN = 64;
 
 var isShift = false;
-var ON_OFF_MESSAGES = ['OFF', 'ON'];
-var MUTE_MESSAGES = ['MUTED', 'LOUD'];
-var BUTTON_MESSAGE_MAP = [ON_OFF_MESSAGES, MUTE_MESSAGES];
-
 var noteIn;
+var cursorTrack;
 
-function cursorTrackNameObserver(n) {
-	host.showPopupNotification('Current track: ' + n);
+function cursorTrackPositionObserver(pos) {
+    host.showPopupNotification('Current track: ['
+        + (pos + 1)
+        + '] '
+        + cursorTrack.name().get()); //behind the curtain: name is updated before position
 }
 
 function init()
 {
-	transport = host.createTransport();
+    transport = host.createTransport();
 
-	// Register callback for midi-events
-	host.getMidiInPort(0).setMidiCallback(onMidi);
+    // Register callback for midi-events
+    host.getMidiInPort(0).setMidiCallback(onMidi);
 
-	// Keyboard
-	noteIn = host.getMidiInPort(0).createNoteInput("Oxygen 88 Keyboard");
-	noteIn.setShouldConsumeEvents(false);
-	
-	// Master track
-	masterTrack = host.createMasterTrack(0);
-	
-	// The cursor
-	cursorTrack = host.createCursorTrack(0, 0);
-	cursorTrack.name().addValueObserver(cursorTrackNameObserver);
-	cursorDevice = cursorTrack.createCursorDevice();	
-	
-	// Knobs 
-	remoteControls = cursorDevice.createCursorRemoteControlsPage(8);
-	for (var i = 0; i < 8; i++)
-	{
-		remoteControls.getParameter(i).setIndication(true);
-	}
-	
-	//Pageable tracks
-	trackBank = host.createMainTrackBank(8, 0, 0);
-	trackBank.followCursorTrack(cursorTrack);
-	
-	for (var i = 0; i < 8; i++)
-	{
-		var bankTrack = trackBank.getChannel(i);
-		bankTrack.name().markInterested();
-		bankTrack.isActivated().markInterested();
-		bankTrack.getMute().markInterested();
-	}
+    // Keyboard
+    noteIn = host.getMidiInPort(0).createNoteInput(OXYGEN_88.NAME + ' Keyboard');
+    noteIn.setShouldConsumeEvents(false);
+
+    // Master track
+    masterTrack = host.createMasterTrack(0);
+
+    // The cursor
+    cursorTrack = host.createCursorTrack(0, 0);
+    cursorTrack.name().markInterested();
+    cursorTrack.position().addValueObserver(cursorTrackPositionObserver);
+
+    cursorDevice = cursorTrack.createCursorDevice();
+
+    // Knobs 
+    remoteControls = cursorDevice.createCursorRemoteControlsPage(KNOB.COUNT);
+    for (var i = 0; i < KNOB.COUNT; i++)    {
+        remoteControls.getParameter(i).setIndication(true);
+    }
+
+    //Pageable tracks
+    trackBank = host.createMainTrackBank(FADER.COUNT, 0, 0);
+    trackBank.followCursorTrack(cursorTrack);
+
+    for (var i = 0; i < FADER.COUNT; i++) {
+        var bankTrack = trackBank.getChannel(i);
+        bankTrack.name().markInterested();
+        bankTrack.isActivated().markInterested();
+        bankTrack.getMute().markInterested();
+        bankTrack.position().markInterested();
+    }
 
 }
 
+function onMidi(status, data1, data2) {
+    printMidi(status, data1, data2);
 
-function idx(b) 
-{
-	return 0 + b;
+    if (isChannelController(status))
+    {
+
+        // Handle transport-buttons and track selection
+        if ((isIn(data1, TRANSPORT.PREV_TRACK, TRANSPORT.RECORD) 
+            && data1 != 112) //112 is skipped in transport messages
+            && data2 > 0) {
+
+            switch(data1) {
+                case TRANSPORT.PREV_TRACK:
+                    cursorTrack.selectPrevious();
+                    break;
+                case TRANSPORT.NEXT_TRACK:
+                    cursorTrack.selectNext();
+                    break;
+                case TRANSPORT.LOOP:
+                    transport.toggleLoop();
+                    break;
+                case TRANSPORT.REWIND:
+                    transport.rewind();
+                    break;
+                case TRANSPORT.FORWARD:
+                    transport.fastForward();
+                    break;
+                case TRANSPORT.STOP:
+                    transport.stop();
+                    break;
+                case TRANSPORT.PLAY:
+                    transport.play();
+                    break;
+                case TRANSPORT.RECORD:
+                    transport.record();
+                    break;
+            }
+
+        // Handle fader for track volume    
+        } else if (isIn(data1, FADER.LOW, FADER.HIGH)) {
+            if (isShift) {
+                //drawbar mode
+                noteIn.sendRawMidiEvent(status, data1, 127-data2);
+            } else {
+                trackBank.getChannel(data1 - FADER.LOW).getVolume().set(data2, D2);
+            }
+        }
+
+        //Handle fader for master volume
+        else if (data1 == FADER.MASTER) {
+            if (isShift) {
+                //drawbar mode
+                noteIn.sendRawMidiEvent(status, data1, 127-data2);
+            } else {
+                masterTrack.getVolume().set(data2, D2);
+            }
+        }
+
+        //Handle arm buttons
+        else if (isIn(data1, BUTTON.LOW, BUTTON.HIGH) && data2 > 0) {
+            var currentTrack = trackBank.getChannel(data1 - BUTTON.LOW);
+            var currentTrackValueHolder = isShift
+                ? currentTrack.getMute()
+                : currentTrack.isActivated();
+            var newValue = !currentTrackValueHolder.get();
+            var isOn = isShift != newValue; //logical XOR
+            if (!isOn) {
+                noteIn.sendRawMidiEvent(S_CC_UNDEFINED, CC_SUSTAIN, 0); //send sustain off
+            }
+            currentTrackValueHolder.set(newValue);
+            host.showPopupNotification(
+                '['
+                + (currentTrack.position().get() + 1)
+                + '] '
+                + currentTrack.name().get() 
+                + ' ' 
+                + BUTTON_MESSAGE_MAP[~~isShift][~~isOn] 
+                );
+        }
+
+        //Handle shift button
+        else if (data1 == BUTTON.SHIFT && data2 > 0) {
+            isShift = !isShift;
+            host.showPopupNotification('Shift ' + MESSAGES.ON_OFF[~~isShift]);
+        } 
+
+        // Handle knobs
+        else if (isIn(data1, KNOB.LOW, KNOB.HIGH)) {
+            var knobIndex = data1 - KNOB.LOW;
+            knobParameter = remoteControls.getParameter(knobIndex);
+            knobParameter.getAmount().set(data2, D2);
+        }
+        
+        //Map Expression to Breath controller, as bitwig somehow gulps Expression messages
+        else if (status == S_CC_UNDEFINED && data1 == CC_EXPRESSION) {
+            noteIn.sendRawMidiEvent(status, 2, data2);
+        }
+    
+    }
 }
 
-function onMidi(status, data1, data2)
-{
-	printMidi(status, data1, data2);
-
-	if (isChannelController(status))
-	{
-		
-		// Handle transport-buttons and track selection
-		if ((data1 >= TRANS.PREV_TRACK && data1 <= TRANS.RECORD && data1 != 112) && data2 > 0)
-		{
-			switch(data1) {
-				case TRANS.PREV_TRACK:
-				cursorTrack.selectPrevious();
-				break;
-			case TRANS.NEXT_TRACK:
-				cursorTrack.selectNext();
-				break;
-			case TRANS.LOOP:
-				transport.toggleLoop();
-				break;
-			case TRANS.REWIND:
-				transport.rewind();
-				break;
-			case TRANS.FORWARD:
-				transport.fastForward();
-				break;
-			case TRANS.STOP:
-				transport.stop();
-				break;
-			case TRANS.PLAY:
-				transport.play();
-				break;
-			case TRANS.RECORD:
-				transport.record();
-				break;
-			}
-		}
-		else
-		{
-			// Handle fader for track volume
-			if (data1 >= LOWEST_FADER && data1 <= HIGHEST_FADER)
-			{
-				trackBank.getChannel(data1 - LOWEST_FADER).getVolume().set(data2, 128);
-			}
-			//Handle fader for master volume
-			else if (data1 == MASTER_FADER) 
-			{
-				masterTrack.getVolume().set(data2, 128);
-			}
-			//Handle arm buttons
-			else if (data1 >= LOWEST_ARM && data1 <= HIGHEST_ARM && data2 > 0) 
-			{
-				var currentTrack = trackBank.getChannel(data1 - LOWEST_ARM);
-				var currentTrackValueHolder = isShift
-					? currentTrack.getMute()
-					: currentTrack.isActivated();
-				var newValue = !currentTrackValueHolder.get();
-				var isOn = isShift != newValue; //logical XOR
-				if (!isOn) {
-					noteIn.sendRawMidiEvent(176, 64, 0); //send sustain off
-				}
-				currentTrackValueHolder.set(newValue);
-				host.showPopupNotification(currentTrack.name().get() + ' ' + BUTTON_MESSAGE_MAP[idx(isShift)][idx(isOn)]);
-			}
-			//Handle shift button
-			else if (data1 == SHIFT_BUTTON && data2 > 0) 
-			{
-				isShift = !isShift;
-				host.showPopupNotification('Shift ' + ON_OFF_MESSAGES[idx(isShift)]);
-			}
-			else
-			{
-				// Handle knobs
-				if (data1 >= LOWEST_KNOB && data1 <= HIGHEST_KNOB)
-				{
-					var knobIndex = data1 - LOWEST_KNOB;
-					knobParameter = remoteControls.getParameter(knobIndex);
-					knobParameter.getAmount().set(data2, 128);
-				}
-			}
-		}
-	}
+function isIn(value, low, high) {
+    return value >= low && value <= high;
 }
 
 function exit()
